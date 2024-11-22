2aeab5a557b0e9cba50b230636288be60be36d31
car020202 <carlitosyanez9@gmail.com>
2024-11-21 22:32:39 -0600

    .

--- src/Causas/VerCausas.jsx
+++ src/Causas/VerCausas.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Navbar from '../navbar/navbar';
 import Footer from '../Footer/Footer';
-import backgroundImage from '../assets/background.jpg';
 
 const ViewCausas = () => {
   const [causas, setCausas] = useState([]);
@@ -18,7 +16,6 @@ const ViewCausas = () => {
         setErrorMessage('No se pudieron cargar las causas');
       }
     };
-    
 
     fetchCausas();
   }, []);
@@ -26,10 +23,19 @@ const ViewCausas = () => {
   return (
     <>
       <div className="button-container" style={{ display: 'flex', justifyContent: 'flex-end', padding: '20px 50px' }}>
-        <button className="create-causa-button" onClick={() => window.location.href = '/crearcausa'} style={{
-          backgroundColor: '#007B8A',
-          color: 'white', border: 'none', padding: '10px 20px', borderRadius: '5px', cursor: 'pointer', fontSize: '20px'
-        }}>
+        <button
+          className="create-causa-button"
+          onClick={() => window.location.href = '/crearcausa'}
+          style={{
+            backgroundColor: '#007B8A',
+            color: 'white',
+            border: 'none',
+            padding: '10px 20px',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            fontSize: '20px',
+          }}
+        >
           Crear Causa
         </button>
       </div>
@@ -47,66 +53,95 @@ const ViewCausas = () => {
           maxWidth: '1600px',
           minHeight: '400px',
           margin: '0 auto',
-          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)'
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
         }}>
           <h2 style={{ textAlign: 'center', marginBottom: '20px', color: '#007c8c' }}>Listado de Causas</h2>
           {errorMessage && <p style={{ color: 'red', textAlign: 'center' }}>{errorMessage}</p>}
           <div className="causas-cards" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}>
-            {causas.map((causa) => (
-              <div key={causa.id} className="causa-card" style={{
-                backgroundColor: 'white',
-                borderRadius: '10px',
-                padding: '20px',
-                width: '450px',
-                boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                textAlign: 'left',
-                position: 'relative',
-                margin: '15px'
-              }}>
-                <img src={`http://localhost:3000${causa.portada}`} alt={`Causa ${causa.nombreCausa}`} style={{ width: '400px', height: '250px', objectFit: 'cover', borderRadius: '10px', marginBottom: '10px' }} />
-                <div style={{ padding: '10px' }}>
-                  <div style={{ position: 'absolute', top: '10px', left: '10px', backgroundColor: 'rgba(0, 0, 0, 0.6)', color: 'white', padding: '5px 10px', borderRadius: '5px', fontSize: '14px' }}>
-                  ${causa.recaudado || 0} recaudado de ${causa.meta || 0} recaudado
-                  </div>
-                  <h3 style={{ marginTop: '10px', fontSize: '20px' }}>{causa.nombreCausa}</h3>
-                  <p style={{ fontSize: '18px', color: '#4a4a4a' }}>{causa.descripcion}</p>
+            {causas.map((causa) => {
+              const progressPercentage = Math.min((causa.recaudado / causa.meta) * 100, 100).toFixed(2);
 
-                  <div style={{ position: 'relative', marginBottom: '10px' }}>
-                    <span style={{
+              return (
+                <div key={causa.id} className="causa-card" style={{
+                  backgroundColor: 'white',
+                  borderRadius: '10px',
+                  padding: '20px',
+                  width: '450px',
+                  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                  textAlign: 'left',
+                  position: 'relative',
+                  margin: '15px',
+                }}>
+                  <img
+                    src={`http://localhost:3000${causa.portada}`}
+                    alt={`Causa ${causa.nombreCausa}`}
+                    style={{ width: '400px', height: '250px', objectFit: 'cover', borderRadius: '10px', marginBottom: '10px' }}
+                  />
+                  <div style={{ padding: '10px' }}>
+                    <div style={{
                       position: 'absolute',
-                      top: '-20px',
-                      left: '0',
-                      color: '#555',
-                      fontSize: '12px',
-                      fontWeight: 'bold'
+                      top: '10px',
+                      left: '10px',
+                      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                      color: 'white',
+                      padding: '5px 10px',
+                      borderRadius: '5px',
+                      fontSize: '14px',
                     }}>
-                      ${causa.recaudado || 0}
-                    </span>
-                    <span style={{
-                      position: 'absolute',
-                      top: '-20px',
-                      right: '0',
-                      color: '#555',
-                      fontSize: '12px',
-                      fontWeight: 'bold'
-                    }}>
-                      ${causa.meta || 0}
-                    </span>
-                    <div style={{ height: '8px', width: '100%', backgroundColor: '#e0e0e0', borderRadius: '5px' }}>
-                      <div style={{ width: `${(causa.recaudado / causa.meta) * 100 || 0}%`, height: '100%', backgroundColor: '#00c853', borderRadius: '5px' }}></div>
+                      ${causa.recaudado || 0} recaudado de ${causa.meta || 0}
                     </div>
-                  </div>
+                    <h3 style={{ marginTop: '10px', fontSize: '20px' }}>{causa.nombreCausa}</h3>
+                    <p style={{ fontSize: '18px', color: '#4a4a4a' }}>{causa.descripcion}</p>
 
-                  <button
-                    className="donate-button"
-                    onClick={() => window.location.href = `/detallecausa/${causa.id}`}
-                    style={{ backgroundColor: '#007B8A', color: 'white', border: 'none', padding: '10px 20px', borderRadius: '5px', cursor: 'pointer' }}
-                  >
-                    Ver
-                  </button>
+                    <div style={{ position: 'relative', marginBottom: '10px' }}>
+                      <span style={{
+                        position: 'absolute',
+                        top: '-20px',
+                        left: '0',
+                        color: '#555',
+                        fontSize: '12px',
+                        fontWeight: 'bold',
+                      }}>
+                        ${causa.recaudado || 0}
+                      </span>
+                      <span style={{
+                        position: 'absolute',
+                        top: '-20px',
+                        right: '0',
+                        color: '#555',
+                        fontSize: '12px',
+                        fontWeight: 'bold',
+                      }}>
+                        ${causa.meta || 0}
+                      </span>
+                      <div style={{ height: '8px', width: '100%', backgroundColor: '#e0e0e0', borderRadius: '5px' }}>
+                        <div style={{
+                          width: `${progressPercentage}%`,
+                          height: '100%',
+                          backgroundColor: '#00c853',
+                          borderRadius: '5px',
+                        }}></div>
+                      </div>
+                    </div>
+
+                    <button
+                      className="donate-button"
+                      onClick={() => window.location.href = `/detallecausa/${causa.id}`}
+                      style={{
+                        backgroundColor: '#007B8A',
+                        color: 'white',
+                        border: 'none',
+                        padding: '10px 20px',
+                        borderRadius: '5px',
+                        cursor: 'pointer',
+                      }}
+                    >
+                      Ver
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </div>
       </div>
